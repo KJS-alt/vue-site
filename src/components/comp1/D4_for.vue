dcb7f164eec7f7c3a5491572da56ce1ff133ac44
KJS-alt <sh170kw@naver.com>
2025-08-25 15:03:38 +0900

    Initial commit

---
+++ src/components/comp1/D4_for.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+const foods = ref(['Apple', 'Banana', 'Cherry'])
+</script>
+
+<template>
+    <ul>
+        <li v-for="food in foods" :key="food">
+            {{ food }}
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
